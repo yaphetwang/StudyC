fe320c75d560193e5bd41e34ad2209d2dab99e29
王亚飞 <wangzifeinn@163.com>
2024-09-15 22:38:55 +0800

    数字之间最长坐标距离，出现次数最多的子串

---
+++ algorithm/hw/GetMaxOccuredSubstring.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+static int str_conut[1000];
+static char *getMaxOccuredSubstring(const char *inputStr)
+{
+
+    int len = strlen(inputStr);
+
+    int left = 0;
+    int right = left + 3;
+    int maxCount = 0;
+    while (right <= len)
+    {
+        char subStr[3];
+        // 截取子串
+        strncpy(subStr, inputStr + left, 3);
+        printf("%s\n", subStr);
+        // int count = 0;
+        // for (int i = left; i < right; i++)
+        // {
+        //     subStr[count++] = inputStr[i];
+        // }
+
+        int str_index = atoi(subStr);
+        str_conut[str_index]++;
+        if (str_conut[str_index] > maxCount)
+        {
+            maxCount = str_conut[str_index];
+        }
+
+        left++;
+        right++;
+    }
+
+    int count = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        if (maxCount == str_conut[i])
+        {
+            count++;
+        }
+    }
+    printf("count:%d\n", count);
+    printf("maxCount:%d\n", maxCount);
+
+    int count_i = 0;
+    int maxOccuredStrLen = count * 4 + count;
+    char *maxOccuredStr = (char *)malloc(maxOccuredStrLen);
+    for (int i = 0; i < 1000; i++)
+    {
+        if (maxCount == str_conut[i])
+        {
+            count_i++;
+            char temp[4];
+            sprintf(temp, "%d", i);
+            // itoa(i, temp, 10); 此函数不存在
+            strcat(maxOccuredStr, temp);
+            // strcat_s(maxOccuredStr, maxOccuredStrLen, temp);
+            if (count_i < count)
+            {
+                strcat(maxOccuredStr, ",");
+            }
+        }
+    }
+
+    return maxOccuredStr;
+}
+
+int main()
+{
+    char inputStr[] = "11212112";
+    // scanf("%s", inputStr);
+    char *maxOccuredStr = getMaxOccuredSubstring(inputStr);
+    printf("%s\n", maxOccuredStr);
+
+    // strstr函数用于在一个字符串中查找另一个字符串首次出现的位置。
+    // 如果找到子字符串，则返回指向子字符串在原字符串中首次出现位置的指针；
+    // 如果未找到，则返回 NULL。
+    char inputStr2[] = "11212112";
+    char *subStr = strstr(inputStr, inputStr2);
+    printf("%ld\n", subStr - inputStr);
+
+    // 如果 s1 和 s2 是相同的，则返回 0；
+    // 如果 s1<s2 则返回小于 0；如果 s1>s2 则返回大于 0。
+    int cmp = strcmp(inputStr, inputStr2);
+    printf("%d\n", cmp);
+
+    return 0;
+}
---
+++ algorithm/hw/MaxDistanceOfIndex.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// 一串数字，找出绝对值差为1的两个数的最大距离
+
+static int GetMaxDistance(const int *numbers, size_t numbersSize)
+{
+    int maxDistance = 0;
+    for (int i = 0; i < numbersSize; i++)
+    {
+        for (int j = i + 1; j < numbersSize; j++)
+        {
+            int distance = abs(numbers[i] - numbers[j]);
+            if (distance == 1)
+            {
+                if (abs(j - i) > maxDistance)
+                {
+                    maxDistance = abs(j - i);
+                }
+            }
+        }
+    }
+
+    return maxDistance;
+}
+
+int main(void)
+{
+    int numbers[] = {6, 4, 2, 5, 1, 3};
+    int numbers2[] = {2, 3, 6, 5, 7, 1, 4};
+    printf("%d\n", GetMaxDistance(numbers, sizeof(numbers) / sizeof(numbers[0])));
+    printf("%d\n", GetMaxDistance(numbers2, sizeof(numbers2) / sizeof(numbers2[0])));
+}
